3cb3de45ddb0df78ebdf88a382d8e455a46487ac
Abhishek Kumar Gupta <107269233+TechieAbhishekGupta@users.noreply.github.com>
2023-02-04 23:55:52 +0530

    Add files via upload
    Adding 8. Write a program to find the factorial of any number.

---
+++ main.c
@@ -0,0 +1,16 @@
+// 10. Write a program to find the factorial of any number
+
+#include <stdio.h>
+
+int main()
+{
+    int num, fact = 1;
+    printf("\n\n\tEnter the no.: ");
+    scanf("%d", &num);
+
+    for(int i=1; i<=num; i++)
+        fact = fact * i;
+    printf("\n\tFactorial of %d = %d", num, fact);
+    printf("\n\n\n");
+    getch();
+}
